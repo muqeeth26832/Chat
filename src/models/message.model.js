63f182984017c3ac7b665ed5bccadf4e7d08c2b9
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2024-07-05 01:09:49 +0530

    fron-end works chk 1

---
+++ src/models/message.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    text: String,
+  },
+  { timestamps: true }
+);
+
+export const Message = mongoose.model("Message", messageSchema);
--- src/websockets/wsServer.js
+++ src/websockets/wsServer.js
@@ -1,9 +1,11 @@
 import { WebSocketServer } from "ws";
 import jwt from "jsonwebtoken";
+import { Message } from "../models/message.model.js";
 export function setupWebSocketServer(server) {
   const wss = new WebSocketServer({ server });
 
   wss.on("connection", (connection, req) => {
+    // read user name and id from cookie
     const cookies = req.headers.cookie;
     if (cookies) {
       const tokenCookieString = cookies
@@ -29,8 +31,34 @@ export function setupWebSocketServer(server) {
         }
       }
     }
-    // check no of clients we are connected to
-    // console.log([...wss.clients].map(c => c.username));
+
+    connection.on("message", async (message) => {
+      const messageData = JSON.parse(message.toString());
+      const { recipient, text } = messageData;
+      if (recipient && text) {
+        const messageDoc = await Message.create({
+          sender: connection.userId,
+          recipient,
+          text,
+        });
+        const timestamp = new Date().toISOString(); // Add timestamp
+        [...wss.clients]
+          .filter((c) => c.userId === recipient)
+          .forEach((c) =>
+            c.send(
+              JSON.stringify({
+                text,
+                timestamp,
+                sender: connection.userId,
+                recipient,
+                id: messageDoc._id, // I am also sending
+              })
+            )
+          );
+      }
+    });
+
+    // check no of clients we are connected to and sent it to all users
     [...wss.clients].forEach((client) => {
       client.send(
         JSON.stringify({
